e9855d3b6c8b0b347bf64fa714b3e0179a9db78b
Xiaoyan Li <lxy.lixiaoyan@gmail.com>
2025-08-19 18:45:03 +0800

    disable module transpilation for babel

--- index.babel.js
+++ index.babel.js
@@ -1,5 +1,3 @@
-"use strict";
-
-require("core-js/modules/es.iterator.constructor.js");
-require("core-js/modules/es.iterator.find.js");
+import "core-js/modules/es.iterator.constructor.js";
+import "core-js/modules/es.iterator.find.js";
 x.find();
